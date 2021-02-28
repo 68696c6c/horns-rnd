92f45b13815c97012ee888b256b5d08d02b98e28
68696c6c <68696c6c@gmail.com>
2021-02-27 17:50:43 -0700

    Add a Gatsby netlify example site.  Not working yet.

--- sites/gatsby-example/src/pages/index.jsx
+++ sites/gatsby-example/src/pages/index.jsx
@@ -13,10 +13,11 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/page-2/">Go to page 2</Link>
+    <br />
     <Button color="primary">Horns button</Button>
   </Layout>
 )
