fbd0d8a11ce8fb55e9ef19ac0467dab900efb0fa
Aaron Hill <68696c6c@gmail.com>
2021-05-07 13:09:19 -0600

    WIP generating theme and writing the result using a template.

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,4 +1,7 @@
+/* eslint-disable no-console */
 import fs from 'fs'
+import path from 'path'
+import ejs from 'ejs'
 
 import { makeTheme, Config } from './config'
 
@@ -6,6 +9,24 @@ export const generateTheme = (
   baseTheme: Partial<Config>,
   outputPath: string,
 ): void => {
-  const result = makeTheme(baseTheme)
-  fs.writeFileSync(outputPath, JSON.stringify(result))
+  console.log('generating theme')
+
+  const filename = path.join(__dirname, '/theme.ejs')
+  console.log('filename', filename)
+
+  const template = fs.readFileSync(filename)
+  console.log('template', template)
+
+  const result = ejs.compile(template.toString(), { filename })
+  console.log('result', result)
+
+  const theme = makeTheme(baseTheme)
+  console.log('theme.name', theme.name)
+
+  ejs.renderFile(outputPath, { theme }, (err, str) => {
+    console.log('err', err)
+    console.log('str', str)
+  })
+
+  // fs.writeFileSync(outputPath, JSON.stringify(result))
 }
